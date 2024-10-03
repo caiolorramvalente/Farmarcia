40cf1e11180c202beedea57828a0d13e826b6360
CAIO LORRAM VALENTE <caio_valente@estudante.sesisenai.org.br>
2024-10-03 17:13:59 -0300

    Realização da pagina de compra (mais so a parte visual)

--- Farmarcia/src/components/FotterRemedios.jsx
+++ Farmarcia/src/components/FotterRemedios.jsx
@@ -1,29 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './BodyRemedios.css'
+import { useNavigate } from 'react-router-dom'
 function FotterRemedios() {
+   const navigate=useNavigate("")
+   const [isOpen,setIsOpen] = useState(false)
+   const [isClosing,setisClosing]=useState("")
+
+   function abrirPopUp(){
+    setIsOpen(true)
+    
+
+   }
+   function fecharPopUp(){
+    setIsOpen(false)
+   }
+
+
   return (
+    <div className='containerPai'>
+
+      {isOpen &&
+      <div className='divPopUp'>
+       <dialog open={isOpen} className={isClosing ?"closing" :"open"}>
+        <div className='divConteudoModal'>
+          <div>
+        <h2>Compra bem sucedida</h2>
+          </div>
+          <div>
+          <button onClick={()=>{setIsOpen(false)}} className='btnFecharModal'>X</button>
+          </div>
+        </div>
+        </dialog>
+      </div>
+      }
+
     <div className='divRemedios'>
         <div className='divDoragina'>
             <img src="../images/doragina.jpg" alt="" />
-            <button className='botaoCompra'>Comprar</button>
+            <button className='botaoCompra' onClick={abrirPopUp}>Comprar</button>
+           
         </div>
 
         <div className='divDorflex'>
-        <img src="../images/dorflex.jpg" alt="" />
-        <button className='botaoCompra'>Comprar</button>
+        <img src="../images/dorflex.jpg" alt="" className='img' />
+        <button className='botaoCompra' onClick={abrirPopUp}>Comprar</button>
         </div>
 
         <div className='divMultigrip'>
-        <img src="../images/Multigrip.jpg" alt="" />
-        <button className='botaoCompra'>Comprar</button>
+        <img src="../images/Multigrip.jpg" alt=""className='img' />
+        <button className='botaoCompra' onClick={abrirPopUp}>Comprar</button>
         </div>
 
         <div className='divMultigrip'>
-        <img src="../images/images.jpg" alt="" />
-        <button className='botaoCompra'>Comprar</button>
+        <img src="../images/images.jpg" alt=""  className='img'/>
+        <button className='botaoCompra' onClick={abrirPopUp}>Comprar</button>
         </div>
-      
     </div>
+        <button className='btnVoltar' onClick={()=>{navigate("/")}}>⬅ Home</button>
+    </div>
+    
   )
 }
 
--- Farmarcia/src/pages/PaginaAtendimentos.jsx
+++ Farmarcia/src/pages/PaginaAtendimentos.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavBarFarmarcia from '../components/NavBarFarmarcia'
-
+import FotterFarmarcia from '../components/FotterFarmarcia'
+import'./PaginaAtendimentos.css'
 function PaginaAtendimentos() {
   return (
     <div>
         <>
         <NavBarFarmarcia />
+        <div className='divAtendimentos'>
+
+
+          
+        </div>
+        <FotterFarmarcia />
         
         
         </>
